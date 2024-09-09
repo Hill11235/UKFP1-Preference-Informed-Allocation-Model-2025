a72fb70da5d8d1047e726b452d2f64c95eae06b4
Hill11235 <ethan.hill@ravelin.com>
2024-09-09 18:48:15 +0100

    further selections logic

--- algo/selections.go
+++ algo/selections.go
@@ -1,6 +1,9 @@
 package algo
 
-// accept ingester and simulate student selections (equal to total of available positions)
+import (
+	"fmt"
+	"sort"
+)
 
 type Selections struct {
 	Rankings []Simulation
@@ -11,11 +14,37 @@ type Simulation struct {
 }
 
 func NewSelections(ingester *Ingester) *Selections {
-	// find total number of available places
 	numPlaces := 0
 
 	for _, val := range ingester.AvailablePositions {
 		numPlaces += val
 	}
+
+	// generate first preferences for each sim based on competition ratios
+	// subsequent places - to start with use relative popularity minus oversubscribed?
 	return nil
+}
+
+func generateSimulationsWithFirstChoice(ingester *Ingester) Selections {
+	selections := Selections{}
+	fmt.Println(ingester.AvailablePositions)
+
+	return selections
+}
+
+func completeSubsequentChoices(selections *Selections, ingester *Ingester) {
+}
+
+func sortRatios(ratios map[string]float32) []string {
+	keys := make([]string, 0, len(ratios))
+
+	for key := range ratios {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return ratios[keys[i]] < ratios[keys[j]]
+	})
+
+	return keys
 }
